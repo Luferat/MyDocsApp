e7b83d5a2175251a2393fe896ea988887656c4e8
André Luferat <contato@luferat.net>
2021-12-21 09:46:54 -0300

    Ajustes e documentação

--- js/global.js
+++ js/global.js
@@ -10,16 +10,22 @@ var btnMenu = el('#menu-toggle');
 // Menu principal
 var menu = el('.wrap>nav');
 
-// Monitora cliques no btnMenu, chamando toggleMenu()
+/**
+ * Monitora cliques no btnMenu, chamando toggleMenu()
+ *  Referências: https://www.w3schools.com/jsref/dom_obj_event.asp
+ */
 btnMenu.onclick = menuToggle;
 
 // Monitora modanças na resolução e redireciona para 'changeRes()'
-window.addEventListener('resize', changeRes);
+window.onresize = changeRes;
 
 // Ação do clique no botão do menu 
 function menuToggle() {
 
-    // Se o menu está visível (display="block"), oculta ele
+    /** 
+     * Se o menu está visível (display="block"), oculta ele.
+     *   Referências: https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+     */
     if (window.getComputedStyle(menu, null).display === 'block') hideMenu();
 
     // Se o menu está oculto (displa="none"), mostra ele
@@ -29,27 +35,36 @@ function menuToggle() {
     return false;
 }
 
-// Mostra menu
+// Mostra menu, troca ícone e altera 'title' do botão.
 function showMenu() {
+
+    /**
+     * Referências:
+     *   https://www.w3schools.com/jsref/dom_obj_style.asp
+     *   https://www.w3schools.com/jsref/prop_element_classlist.asp
+     *   https://www.w3schools.com/jsref/met_element_setattribute.asp     * 
+     */
+
     menu.style.display = 'block';
-    // btnMenu.innerHTML = '<i class="fas fa-ellipsis-h fa-fw"></i>';
-    btnMenu.setAttribute('title', 'Oculta o menu');
     btnMenu.classList.add('fa-rotate-90');
+    btnMenu.setAttribute('title', 'Oculta o menu');
 }
 
-// Oculta menu
+// Oculta menu, troca ícone e altera 'title' do botão.
 function hideMenu() {
     menu.style.display = 'none';
-    // btnMenu.innerHTML = '<i class="fas fa-ellipsis-v fa-fw"></i>';
-    btnMenu.setAttribute('title', 'Mostra o menu');
     btnMenu.classList.remove('fa-rotate-90');
+    btnMenu.setAttribute('title', 'Mostra o menu');
 }
 
 // Processa mudanças na resolução
 function changeRes() {
-    
-    // Se a resolução é maior, sempre mostra o menu
-    if(document.documentElement.clientWidth > 767) showMenu();
+
+    /**
+     * Se a resolução é maior, sempre mostra o menu.
+     *   Referências: https://www.w3schools.com/jsref/prop_document_documentelement.asp
+     */
+    if (document.documentElement.clientWidth > 767) showMenu();
 
     // Se a resolução é menor, sempre oculta o menu
     else hideMenu();
@@ -58,12 +73,18 @@ function changeRes() {
     return false;
 }
 
-// Atalho para document.querySelector()
+/**
+ * Atalho para document.querySelector()
+ *   Referências: https://www.w3schools.com/jsref/met_document_queryselector.asp
+ */
 function el(selector) {
     return document.querySelector(selector);
 }
 
-// Atalho para document.querySelectorAll()
+/**
+ * Atalho para document.querySelectorAll()
+ *   Referências: https://www.w3schools.com/jsref/met_document_queryselectorall.asp
+ */
 function els(selector) {
     return document.querySelectorAll(selector);
 }
